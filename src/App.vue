aad3bb7e333b3c7c26349ace4523a5ac79659daf
Hojiakbar Zokirov <hojiakbar312@gmail.com>
2023-08-24 19:37:47 +0500

    CODE RUB:PASS

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="container">
+      <div class="text-center">
+        <h2 class="text-center mt-5">Trending Movies 🍿</h2>
+        <p>Keep up with the hottest movies that are trending this week.</p>
+      </div>
+
+      <div class="my-4 text-center">
+        <a href="#" @click="getTrendingMovies('day')" class="mx-3 h4">
+          Trending today</a
+        >
+        <a href="#" @click="getTrendingMovies('week')" class="mx-3 h4"
+          >This week</a
+        >
+      </div>
+
+      <div class="row" v-if="movies.length > 0">
+        <div class="col-md-3" v-for="(movie, i) in movies" :key="i">
+          <movie-card :movie="movie" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MovieCard from "./components/MovieCard.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    MovieCard,
+  },
+  data() {
+    return {
+      movies: [],
+      apiKey: "0d21b89363c6ce7ff4c0da9ba1670e4a",
+    };
+  },
+  methods: {
+    getTrendingMovies(category) {
+      return fetch(
+        `https://api.themoviedb.org/3/trending/movie/${category}?api_key=${this.apiKey}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          this.movies = data.results;
+        });
+    },
+  },
+  mounted() {
+    this.getTrendingMovies("day");
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
